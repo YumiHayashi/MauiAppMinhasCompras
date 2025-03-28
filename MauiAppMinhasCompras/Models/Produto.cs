95a2eac8fc6558294c16085b05d8bc971a337f3b
Srta. Hayashi <129611813+YumiHayashi@users.noreply.github.com>
2025-03-28 13:10:04 -0300

    Update Produto.cs

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,4 +1,7 @@
-﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
@@ -9,6 +12,34 @@ namespace MauiAppMinhasCompras.Models
         public string Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
+        public string Categoria { get; set; } // Novo campo
         public double Total { get => Quantidade * Preco; }
+    }
+
+    public class Compra
+    {
+        public List<Produto> Produtos { get; set; }
+
+        public Compra()
+        {
+            Produtos = new List<Produto>();
+        }
+
+        public void AdicionarProduto(Produto produto)
+        {
+            Produtos.Add(produto);
+        }
+
+        public void ExibirRelatorioPorCategoria()
+        {
+            var relatorio = Produtos.GroupBy(p => p.Categoria)
+                .Select(g => new { Categoria = g.Key, TotalGasto = g.Sum(p => p.Total) });
+
+            Console.WriteLine("--- Relatório de Compras por Categoria ---");
+            foreach (var item in relatorio)
+            {
+                Console.WriteLine($"Categoria: {item.Categoria}, Total Gasto: R${item.TotalGasto}");
+            }
+        }
     }
 }
